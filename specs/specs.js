f35aca9d39bbfa71a7b70e149718bda146eb7303
Brian Borealis <brian.borealis@gmail.com>
2015-09-11 09:13:39 -0700

    third fourt and fifth test pass

--- specs/specs.js
+++ specs/specs.js
@@ -7,5 +7,17 @@ describe('findAndReplace', function() {
         expect(findAndReplace("a", "a", "b")).to.be.equal("b");
     });
 
+    it("enters a two word input string, a find string, and a replace string and recieves an ouput of the original input string with the word replaced", function() {
+        expect(findAndReplace("Hello world", "world", "universe")).to.be.equal("Hello universe");
+    });
+
+    it("enters a three word input string, a find string, and a replace string and recieves an ouput of the original input string with the word replaced", function() {
+        expect(findAndReplace("Hello mister world", "world", "universe")).to.be.equal("Hello mister universe");
+    });
+
+    it("enters a three word input string with punctuation, a find string, and a replace string and recieves an ouput of the original input string with the word replaced. ", function() {
+        expect(findAndReplace("Hello mister world.", "world", "universe")).to.be.equal("Hello mister universe.");
+    });
+
 
 });
